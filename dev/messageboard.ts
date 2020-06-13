3e1bcf079438d4832b8c9f56a09beae6c930dd6b
Bryan <bryanderonde@hotmail.com>
2020-06-13 17:12:46 +0200

    add the singleton pattern

--- dev/messageboard.ts
+++ dev/messageboard.ts
@@ -1,11 +1,14 @@
 class MessageBoard extends HTMLElement{
 
     
-
+    private static _instance : MessageBoard
     public x : number = 0;
     public y : number = 0;
-
     
+    public static instance() : MessageBoard {
+        if(!MessageBoard._instance) MessageBoard._instance = new MessageBoard();
+        return MessageBoard._instance;
+    }
     constructor() {
         super()
 
